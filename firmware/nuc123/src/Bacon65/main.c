65d31dcd7b67ad53e258a711b2be702d62d809d3
ProgCat <progcat@protonmail.com>
2021-12-16 04:24:11 +0800

    wrote some delay function, haven't test yet

--- firmware/nuc123/src/Bacon65/main.c
+++ firmware/nuc123/src/Bacon65/main.c
@@ -4,25 +4,15 @@
 
 void SYS_Init(void)
 {
-    /* Enable IP clock */
-    CLK->APBCLK = CLK_APBCLK_UART0_EN_Msk;
-
-    /* Update System Core Clock */
-    /* User can use SystemCoreClockUpdate() to calculate SystemCoreClock and cyclesPerUs automatically. */
     SystemCoreClockUpdate();
-
 }
 
 
 int main()
 {
-
-    /* Unlock protected registers */
+	// Initialize the system
     SYS_UnlockReg();
-
     SYS_Init();
-
-    /* Lock protected registers */
     SYS_LockReg();
 
 }
