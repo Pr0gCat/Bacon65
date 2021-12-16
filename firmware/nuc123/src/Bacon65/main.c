970b345721063d2f6a837914d1535de3bf23f182
ProgCat <progcat@protonmail.com>
2021-12-16 18:41:00 +0800

    I need to know am I doing right

--- firmware/nuc123/src/Bacon65/main.c
+++ firmware/nuc123/src/Bacon65/main.c
@@ -14,5 +14,17 @@ int main()
     SYS_UnlockReg();
     SYS_Init();
     SYS_LockReg();
+	// if the delete key pressed, the L4 layer will be high
+    GPIO_SetMode(PD, BIT11, GPIO_PMD_OUTPUT); // scan L1
+	GPIO_SetMode(PB, BIT6, GPIO_PMD_OUTPUT); // L4 scan line
+	GPIO_SetMode(PF, BIT0, GPIO_PMD_INPUT);
 
+	PD11 = 1;
+	while(1){
+		if(PF0){
+				PB6 = 1;
+		}else{
+				PB6 = 0;
+		}
+	}
 }
